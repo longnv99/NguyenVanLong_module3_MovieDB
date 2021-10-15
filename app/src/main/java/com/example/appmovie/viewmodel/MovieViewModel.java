27008bd453cafdb3c6c4a9d374f4519f0359cee5
longnv99 <litlong99@gmail.com>
2021-10-15 11:15:47 +0700

    update search using PublishSubject and Disposable

--- app/src/main/java/com/example/appmovie/viewmodel/MovieViewModel.java
+++ app/src/main/java/com/example/appmovie/viewmodel/MovieViewModel.java
@@ -1,5 +1,9 @@
 package com.example.appmovie.viewmodel;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
@@ -9,6 +13,9 @@ import com.example.appmovie.model.searchmodel.ResultSearch;
 import com.example.appmovie.repository.MovieRepository;
 import com.example.appmovie.responses.MovieResponse;
 import com.example.appmovie.responses.MovieResultSearchResponse;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public
 class MovieViewModel extends ViewModel {
@@ -25,15 +32,4 @@ class MovieViewModel extends ViewModel {
     LiveData<Object> getDataMovie(String api_key, String language, int page) {
         return movieRepository.getDataMovie(api_key, language, page);
     }
-
-    public
-    LiveData<ResultSearch> getResultSearch(String api_key, String query, int page) {
-        return movieRepository.getResultSearch(api_key, query, page);
-    }
-
-    public
-    LiveData<MovieResultSearchResponse> getMovieResultSearch(String api_key, String query) {
-        return movieRepository.getMovieResultSearch(api_key, query);
-    }
-
 }
